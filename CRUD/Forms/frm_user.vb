7bbae3125249dab96cf31a60bf8df401c0e683e5
aumengse <balanayaerole@gmail.com>
2017-02-17 14:08:17 +0800

    change path

---
+++ CRUD/Forms/frm_user.vb
@@ -0,0 +1,14 @@
+﻿Public Class frm_users
+
+    Private Sub TextBox1_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles txtid.TextChanged
+
+    End Sub
+
+    Private Sub Label2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Label2.Click
+
+    End Sub
+
+    Private Sub Label3_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Label3.Click
+
+    End Sub
+End Class
